b6e536b751264630d3aa0d3f45937d6642323b88
kauefelippet <kaue.andrea.elias@gmail.com>
2025-03-25 22:25:39 -0300

    adds CSS Aula_06 and its exercises

--- Semestre_3/Estrutura_de_Dados/Aula_06/LinkedList/IntNoSimples.java
+++ Semestre_3/Estrutura_de_Dados/Aula_06/LinkedList/IntNoSimples.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package exemplolista;
+package Semestre_3.Estrutura_de_Dados.Aula_06.LinkedList;
 
 /**
  *
--- Semestre_3/Estrutura_de_Dados/Aula_06/LinkedList/ListaEncadeada.java
+++ Semestre_3/Estrutura_de_Dados/Aula_06/LinkedList/ListaEncadeada.java
@@ -2,128 +2,120 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package exemplolista;
+package Semestre_3.Estrutura_de_Dados.Aula_06.LinkedList;
 
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author bruno
  */
 public class ListaEncadeada {
     IntNoSimples primeiro, ultimo;
-    int numero_nos=0;
-    
-    ListaEncadeada(){
-        primeiro = ultimo = null;   
+    int numero_nos = 0;
+
+    ListaEncadeada() {
+        primeiro = ultimo = null;
     }
-    void insereNo_fim (IntNoSimples novoNo){
-	novoNo.prox = null;
+
+    void insereNo_fim(IntNoSimples novoNo) {
+        novoNo.prox = null;
         if (primeiro == null)
             primeiro = novoNo;
-	if (ultimo != null)
+        if (ultimo != null)
             ultimo.prox = novoNo;
         ultimo = novoNo;
-	numero_nos++;
-    }
-    void insereNo_inicio (IntNoSimples novoNo){
-        novoNo.prox = primeiro;
-	if(primeiro == null && ultimo==null) //Só tem um elemento na lista
-        {
-            ultimo = novoNo;
-	}
-	primeiro = novoNo;
         numero_nos++;
     }
-    int ContarNos(){
-	int tamanho = 0;
+
+    void insereNo_inicio(IntNoSimples novoNo) {
+        novoNo.prox = primeiro;
+        if (primeiro == null && ultimo == null) //Só tem um elemento na lista
+        {
+            ultimo = novoNo;
+        }
+        primeiro = novoNo;
+        numero_nos++;
+    }
+
+    int ContarNos() {
+        int tamanho = 0;
         IntNoSimples temp_no = primeiro;
-    	while (temp_no != null)
-        {   tamanho++;
+        while (temp_no != null) {
+            tamanho++;
             temp_no = temp_no.prox;
-	}
+        }
         return tamanho;
     }
-    void insereNo_posicao(IntNoSimples novoNo, int posicao){
-	IntNoSimples temp_no = primeiro;
-	int numero_nos = ContarNos();
-	int pos_aux;
-	if(posicao == 0)
-	{
+
+    void insereNo_posicao(IntNoSimples novoNo, int posicao) {
+        IntNoSimples temp_no = primeiro;
+        int numero_nos = ContarNos();
+        int pos_aux;
+        if (posicao == 0) {
             novoNo.prox = primeiro;
-            if(primeiro == ultimo)
-            {
+            if (primeiro == ultimo) {
                 ultimo = novoNo;
             }
             primeiro = novoNo;
-        }
-        else
-	{
-            if (posicao <= numero_nos)
-            {   
-		pos_aux = 1;
-		while(temp_no != null && posicao > pos_aux)
-		{
+        } else {
+            if (posicao <= numero_nos) {
+                pos_aux = 1;
+                while (temp_no != null && posicao > pos_aux) {
                     temp_no = temp_no.prox;
-                    pos_aux ++;
+                    pos_aux++;
                 }
                 novoNo.prox = temp_no.prox;
                 temp_no.prox = novoNo;
-            }
-            else
-            {
-                if(posicao > numero_nos)
-		{
+            } else {
+                if (posicao > numero_nos) {
                     ultimo.prox = novoNo;
                     ultimo = novoNo;
-		}
-            }	 
+                }
+            }
         }
     }
 
 
-    IntNoSimples buscaNo (int buscaValor){
+    IntNoSimples buscaNo(int buscaValor) {
         int i = 0;
         IntNoSimples temp_no = primeiro;
-        while (temp_no != null)
-        {
-            if (temp_no.valor == buscaValor)
-            {
+        while (temp_no != null) {
+            if (temp_no.valor == buscaValor) {
                 JOptionPane.showMessageDialog(null, "No " + temp_no.valor + " posição " + i);
-                return temp_no;	
+                return temp_no;
             }
             i++;
             temp_no = temp_no.prox;
         }
         return null;
     }
-    void excluiNo (int valor){
+
+    void excluiNo(int valor) {
         IntNoSimples temp_no = primeiro;
-        IntNoSimples anterior_no=null;
-        while (temp_no != null && temp_no.valor != valor){
+        IntNoSimples anterior_no = null;
+        while (temp_no != null && temp_no.valor != valor) {
             anterior_no = temp_no;
             temp_no = temp_no.prox;
         }
-        if (temp_no == primeiro){
-            if (temp_no.prox !=null)
+        if (temp_no == primeiro) {
+            if (temp_no.prox != null)
                 primeiro = temp_no.prox;
             else
                 primeiro = null;
+        } else {
+            anterior_no.prox = temp_no.prox;
         }
-        else{
-            anterior_no.prox =temp_no.prox;
-        }
-        
+
         if (ultimo == temp_no)
             ultimo = anterior_no;
     }
-    void exibeLista(){
+
+    void exibeLista() {
         IntNoSimples temp_no = primeiro;
         int i = 0;
-        while (temp_no != null)
-        {
+        while (temp_no != null) {
             System.out.println("Saida - Valor" + temp_no.valor + " posição " + i);
-                            temp_no = temp_no.prox;
+            temp_no = temp_no.prox;
             i++;
         }
     }
