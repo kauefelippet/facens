4479434400a3af63c1232cb72a01f34be4515425
kauefelippet <kaue.andrea.elias@gmail.com>
2025-08-18 22:18:57 -0300

    starts Semestre_4

---
+++ Semestre_4/Programacao_Front_End/AulaAngular_1/src/app/destaque/destaque.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destaque',
+  standalone: false,
+  templateUrl: './destaque.component.html',
+  styleUrl: './destaque.component.css'
+})
+export class DestaqueComponent {
+
+}
---
+++ Semestre_4/Programacao_Front_End/AulaAngular_1/src/app/noticia/noticia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noticia',
+  standalone: false,
+  templateUrl: './noticia.component.html',
+  styleUrl: './noticia.component.css'
+})
+export class NoticiaComponent {
+
+}
